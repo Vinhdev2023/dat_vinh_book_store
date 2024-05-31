30b738345528aa22e9ac24d6086832f3dcba3b30
Vinhdev2023 <daothanhvinh2004@gmail.com>
2024-06-01 00:07:13 +0700

    vinh add data table

---
+++ resources/views/AdminPages/AdminCategoriesData.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <link rel="icon" href="/apple-touch-icon.png">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/admin_plugin/plugins/fontawesome-free/css/all.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="/admin_plugin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/admin_plugin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/admin_plugin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/admin_plugin/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+    <!-- Navbar -->
+    @include('AdminPages.AdminTaskbar.Navbar')
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    @include('AdminPages.AdminTaskbar.MainSidebarContainer')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Product DataTables</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Product DataTables</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">DataTable with minimal features & hover style</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example2" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Rendering engine</th>
+                                            <th>Browser</th>
+                                            <th>Platform(s)</th>
+                                            <th>Engine version</th>
+                                            <th>CSS grade</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Other browsers</td>
+                                            <td>All others</td>
+                                            <td>-</td>
+                                            <td>-</td>
+                                            <td>U</td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Rendering engine</th>
+                                            <th>Browser</th>
+                                            <th>Platform(s)</th>
+                                            <th>Engine version</th>
+                                            <th>CSS grade</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    @include('AdminPages.AdminTaskbar.Footer')
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="/admin_plugin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/admin_plugin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="/admin_plugin/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="/admin_plugin/plugins/jszip/jszip.min.js"></script>
+<script src="/admin_plugin/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="/admin_plugin/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="/admin_plugin/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="/admin_plugin/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/admin_plugin/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/admin_plugin/dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+    $(function () {
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
+</body>
+</html>
